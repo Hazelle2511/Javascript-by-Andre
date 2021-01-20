89f10e711f8a8914a9e9f23ade5c1a69d7e069ae
Hazelle2511 <camacho.hazelle@gmail.com>
2021-01-20 14:46:48 +0100

    functions

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-4 + 5;
-if (4 + 5 === 9) {
-  alert("You are smart");
+// 4 + 5;
+// if (4 + 5 === 9) {
+//   alert("You are smart");
+// }
+
+///118 Functions
+
+//Function Declaration
+
+function sayHello() {
+  console.log("Hello");
 }
+sayHello();
+
+//Function Expression
+//Anonymous Function
+var sayBye = function () {
+  console.log("bye");
+};
+
+sayBye();
+
+function sing(song) {
+  console.log(song);
+}
+
+sing("Thank You Next");
+sing("Thank You Next");
