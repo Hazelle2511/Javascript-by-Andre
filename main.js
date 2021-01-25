0d65cde9cfa5fcb0ac65a4a129f3952f0008d2f3
Hazelle2511 <camacho.hazelle@gmail.com>
2021-01-25 20:48:22 +0100

    functions

--- main.js
+++ main.js
@@ -26,3 +26,26 @@ function sing(song) {
 
 sing("Thank You Next");
 sing("Thank You Next");
+
+// function multiply(a, b) {
+//   console.log(a, b);
+//   return a * b;
+// }
+// multiply(5, 10);
+
+//
+function multiply(a, b) {
+  if (a > 10 || b > 10) {
+    return "that's too hard";
+  } else {
+    return a * b;
+  }
+}
+
+//
+function multiply(a, b) {
+  return a * b;
+}
+alert(multiply(5, 10));
+
+//
