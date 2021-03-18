90510afc689208fddad5b1d6d2e1287d2bc2514e
Hazelle2511 <camacho.hazelle@gmail.com>
2021-03-18 08:38:07 +0100

    dom

--- DOM/main.js
+++ DOM/main.js
@@ -14,5 +14,35 @@ dom = document.querySelector("li").setAttribute("random", "1000");
 //Changing styles
 //style.{property}//ok
 
-dom = document.querySelector("h1").style.backgroundColor = "yellow";
-console.log(dom);
+// dom = document.querySelector("h1").style.backgroundColor = "yellow";
+// console.log(dom);
+
+//className//best
+
+// var h1 = document.querySelector("h1");
+// h1.className = "classTitle";
+
+//classList//best
+let li;
+li = document.querySelector("li").classList;
+li = document.querySelector("li").classList.add("classTitle");
+li = document.querySelector("li").classList.remove("classTitle");
+li = document.querySelector("li").classList.add("done");
+li = document.querySelector("li").classList.toggle("done");
+
+//Bonus
+//innerHTML//dangerous
+let h1;
+h1 = document.querySelector("h1").innerHTML = "<strong>!!!!!!</strong>";
+
+//parent element
+li = document.querySelectorAll("li")[1];
+li = document.querySelectorAll("li")[1].parentElement;
+li = document.querySelectorAll("li")[1].parentElement.parentElement;
+
+//children
+li = document.querySelectorAll("li")[1].parentElement.parentElement.children;
+console.log(li);
+
+//IT IS  IMPORTANT TO CACHE SELECTORS IN VARIABLES
+var h1 = document.querySelector("h1");
