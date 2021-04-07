d565dfb7e9ca70389604213f5b589393cd075570
Hazelle2511 <camacho.hazelle@gmail.com>
2021-04-07 12:01:51 +0200

    ternaryOperator&switch

--- AdvancedJS/main.js
+++ AdvancedJS/main.js
@@ -37,3 +37,58 @@ funFunction();
 funerFunction();
 funestFunction();
 console.log(fun)
+
+
+//144. Advanced Control Flow
+
+//condition ? expr1 : expr2
+
+
+//ternary operator
+function areYouaFan(boolean) {
+    return boolean;
+}
+
+var  answer = areYouaFan(true)? 'Yes, I am fan of LBJ' :  'Not at all';
+
+var automatedAnswer = "Your player #  is" + ( areYouaFan(true) ? "23" :"isInvalid");
+
+
+function validate() {
+    if(areYouaFan(true)) {
+        return "Yes, I am fan of LBJ"
+    }
+
+    else {
+        return "Not at all"
+    }
+}
+
+var answer1 = validate();
+
+//Switch
+
+function moveCommand(direction) {
+    var whatHappens;
+    switch(direction) {
+        case "forward":
+            whatHappens = "You arrived home";
+            break;
+        
+        case "backward":
+            whatHappens = "You encountered a bull";
+            break;
+        
+        case "right":
+            whatHappens = "You found a river";
+        break;
+
+        case "left":
+            whatHappens = "You run into a troll";
+            break;
+        default:
+            whatHappens = "Please enter a valid direction";
+
+    }
+    return whatHappens;
+}
